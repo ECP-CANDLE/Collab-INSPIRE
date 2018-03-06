965f7d6bb379aa27d353845b0acc8003cd0c702f
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-03-06 10:11:46 -0600

    Fix comment syntax

--- tests/namd-1.swift
+++ tests/namd-1.swift
@@ -1,14 +1,14 @@
 
-# The output and input files of the simulation app varies from the type of
-# `stage` that we are in.
+// The output and input files of the simulation app varies from the type of
+// `stage` that we are in.
 
 app (file coor, file vel, file xsc) simulation (file stage, file system, file vel, file xsc, file topology, file constraints)
 {
-  # Cd into the correct folder and copy/link all input files in there
-  # How is this done?
+  // Cd into the correct folder and copy/link all input files in there
+  // How is this done?
 
-  # Run the namd executable with core mapping options and the customised
-  # input configuration file.
+  // Run the namd executable with core mapping options and the customised
+  // input configuration file.
   bash namd2 +ppn 15 +pemap 1-15 +commap 0 stage.conf stdout=@out
 }
 
@@ -19,7 +19,7 @@ mutation_systems = ['MUT1', 'MUT2', 'MUT3', 'MUT4', 'MUT5', 'MUT6']
 foreach replica in [0:number_of_replicas-1] {
   foreach mutation in mutation_systems {
 
-    (coor, vel, xsc) = simulation('minimize', mutation) # arguments are empty here! is this okay?
+    (coor, vel, xsc) = simulation('minimize', mutation) // arguments are empty here! is this okay?
     (coor, vel, xsc) = simulation('heat', coor, top, vel, xsc)
     (coor, vel, xsc) = simulation('equilibrate', coor, top, vel, xsc)
     simulation('production', coor, top, vel, xsc)
