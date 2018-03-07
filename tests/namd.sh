8b7de6379f4c9b19235a72f941029ae116c876df
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-03-06 22:39:40 -0600

    Disable module command for this test script

--- tests/namd.sh
+++ tests/namd.sh
@@ -7,7 +7,8 @@ set -eu
 # This is how we load NAMD on Titan. These instruction are all coming from
 # the Titan website. This might have to go in the bash submission script.
 
-module load namd/2.12
+# module load namd/2.12
+
 export MPICH_PTL_SEND_CREDITS=-1
 export MPICH_MAX_SHORT_MSG_SIZE=8000
 export MPICH_PTL_UNEX_EVENTS=80000
