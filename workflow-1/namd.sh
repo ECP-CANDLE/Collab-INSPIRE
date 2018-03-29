0132ba1da9a2086a1f9869fa9682a37cccf56b7a
Justin M Wozniak <wozniak@mcs.anl.gov>
2018-03-28 23:16:17 -0500

    Update namd.sh header notes and settings

--- workflow-1/namd.sh
+++ workflow-1/namd.sh
@@ -4,15 +4,8 @@ set -eu
 # NAMD SH
 # Wrapper script around NAMD
 
-# This is how we load NAMD on Titan. These instruction are all coming from
-# the Titan website. This might have to go in the bash submission script.
-
+# This is from the NAMD2 binary download
 NAMD2=/lustre/atlas/proj-shared/csc249/sfw/NAMD_2.12_Linux-x86_64-multicore/namd2
-
-export MPICH_PTL_SEND_CREDITS=-1
-export MPICH_MAX_SHORT_MSG_SIZE=8000
-export MPICH_PTL_UNEX_EVENTS=80000
-export MPICH_UNEX_BUFFER_SIZE=100M
 
 # In theory this is the only application that we need for now. Maybe
 # one ore more analysis steps will follow. The Simulation application
